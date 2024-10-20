1e121c708452e99873abe9ec73a77677f565562b
Abderrahmen-GHROUBI <abderrahmen.ghroubi@enicar.ucar.tn>
2024-10-20 04:35:40 +0100

    Add db

---
+++ drizzle.config.js
@@ -0,0 +1,12 @@
+
+
+export default {
+    dialect: "postgresql",
+    schema: "./utils/schema.jsx",
+    out: "./drizzle",
+    dbCredentials: {
+      url: "postgresql://FinanSmart_owner:r1tILVGgO0MT@ep-tiny-union-a5wfe08h.us-east-2.aws.neon.tech/FinanSmart?sslmode=require",
+      connectionString:
+        "postgresql://FinanSmart_owner:r1tILVGgO0MT@ep-tiny-union-a5wfe08h.us-east-2.aws.neon.tech/FinanSmart?sslmode=require",
+    },
+  };
